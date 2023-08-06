eef75eb9277afcce7f621c92dedaa31afe6e1899
Colin Hughes <colin@w4b.co.uk>
2023-08-06 17:01:57 +0100

    removed .net framework projects from solution

--- src/KeyStreamCaptureCore/MainWindow.xaml.cs
+++ src/KeyStreamCaptureCore/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Forms;
 
 namespace KeyStreamCapture {
     public partial class MainWindow {
-        private readonly List<InterceptKeyEventArgs> keys = new List<InterceptKeyEventArgs>();
+        private readonly List<InterceptKeyEventArgs> keys = new();
         private readonly IDisposable subscription;
         private bool capturing;
 
@@ -38,39 +38,38 @@ namespace KeyStreamCapture {
 
         private void GenerateCode() {
             _ = keys.ToObservable();
-            CodeGeneratorOptions cgo = new CodeGeneratorOptions {
+            CodeGeneratorOptions cgo = new() {
                 BracingStyle = "C",
                 BlankLinesBetweenMembers = false
             };
-            using (CSharpCodeProvider provider = new CSharpCodeProvider()) {
-                CodeMemberMethod method = new CodeMemberMethod {
-                    Name = "KeyStream",
-                    ReturnType = new CodeTypeReference(typeof(IObservable<InterceptKeyEventArgs>))
-                };
+            using CSharpCodeProvider provider = new();
+            CodeMemberMethod method = new() {
+                Name = "KeyStream",
+                ReturnType = new CodeTypeReference(typeof(IObservable<InterceptKeyEventArgs>))
+            };
 
-                CodeVariableDeclarationStatement player =
-                    new CodeVariableDeclarationStatement(new CodeTypeReference(typeof(KeyPlayer)), "keys",
-                    new CodeObjectCreateExpression(typeof(KeyPlayer)));
-                _ = method.Statements.Add(player);
-                foreach (InterceptKeyEventArgs interceptKeyEventArgse in keys) {
-                    CodeObjectCreateExpression key = new CodeObjectCreateExpression(new CodeTypeReference(typeof(InterceptKeyEventArgs)),
-                    new CodePropertyReferenceExpression(new CodeTypeReferenceExpression(typeof(Keys)), interceptKeyEventArgse.Key.ToString()),
-                    new CodePropertyReferenceExpression(new CodeTypeReferenceExpression(typeof(KeyDirection)), interceptKeyEventArgse.KeyDirection.ToString()),
-                    new CodePrimitiveExpression(interceptKeyEventArgse.AltPressed),
-                    new CodePrimitiveExpression(interceptKeyEventArgse.ControlPressed),
-                    new CodePrimitiveExpression(interceptKeyEventArgse.ShiftPressed));
+            CodeVariableDeclarationStatement player =
+                new(new CodeTypeReference(typeof(KeyPlayer)), "keys",
+                new CodeObjectCreateExpression(typeof(KeyPlayer)));
+            _ = method.Statements.Add(player);
+            foreach (InterceptKeyEventArgs interceptKeyEventArgs in keys) {
+                CodeObjectCreateExpression key = new(new CodeTypeReference(typeof(InterceptKeyEventArgs)),
+                new CodePropertyReferenceExpression(new CodeTypeReferenceExpression(typeof(Keys)), interceptKeyEventArgs.Key.ToString()),
+                new CodePropertyReferenceExpression(new CodeTypeReferenceExpression(typeof(KeyDirection)), interceptKeyEventArgs.KeyDirection.ToString()),
+                new CodePrimitiveExpression(interceptKeyEventArgs.AltPressed),
+                new CodePrimitiveExpression(interceptKeyEventArgs.ControlPressed),
+                new CodePrimitiveExpression(interceptKeyEventArgs.ShiftPressed));
 
-                    CodeMethodInvokeExpression keyPress = new CodeMethodInvokeExpression(new CodeVariableReferenceExpression("keys"), "Add", key);
-                    _ = method.Statements.Add(keyPress);
-                }
-                _ = method.Statements.Add(new CodeMethodReturnStatement(new CodeVariableReferenceExpression("keys")));
-
-                StringBuilder sb = new StringBuilder();
-                using (StringWriter stringWriter = new StringWriter(sb)) {
-                    provider.GenerateCodeFromMember(method, stringWriter, cgo);
-                }
-                textBox.Text = sb.ToString();
+                CodeMethodInvokeExpression keyPress = new(new CodeVariableReferenceExpression("keys"), "Add", key);
+                _ = method.Statements.Add(keyPress);
             }
+            _ = method.Statements.Add(new CodeMethodReturnStatement(new CodeVariableReferenceExpression("keys")));
+
+            StringBuilder sb = new();
+            using (StringWriter stringWriter = new(sb)) {
+                provider.GenerateCodeFromMember(method, stringWriter, cgo);
+            }
+            textBox.Text = sb.ToString();
         }
 
         protected override void OnClosed(EventArgs e) {

ea0974c75e78260392bc53a3a1465d1b64952092
Colin Hughes <colin@w4b.co.uk>
2024-10-18 13:19:50 +0100

    create ThreeColumnControl to replace PreferencesField & use in General

---
+++ src/w4b.carnac/UI/ThreeColumnControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace w4b.carnac.UI {
+    /// <summary>
+    /// Interaction logic for ThreeColumnControl.xaml
+    /// </summary>
+    public partial class ThreeColumnControl: UserControl {
+        public static readonly DependencyProperty LabelTextProperty =
+            DependencyProperty.Register("LabelText", typeof(string), typeof(ThreeColumnControl), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty MainContentProperty =
+            DependencyProperty.Register("MainContent", typeof(object), typeof(ThreeColumnControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty OptionalContentProperty =
+            DependencyProperty.Register("OptionalContent", typeof(object), typeof(ThreeColumnControl), new PropertyMetadata(null));
+
+        public string LabelText {
+            get => (string)GetValue(LabelTextProperty);
+            set => SetValue(LabelTextProperty, value);
+        }
+
+        public object MainContent {
+            get => GetValue(MainContentProperty);
+            set => SetValue(MainContentProperty, value);
+        }
+
+        public object OptionalContent {
+            get => GetValue(OptionalContentProperty);
+            set => SetValue(OptionalContentProperty, value);
+        }
+
+        public ThreeColumnControl() {
+            InitializeComponent();
+        }
+    }
+}
